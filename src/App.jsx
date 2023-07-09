59b74f3c0d24b06d42382f814e676efc2c5c6e07
EmanuelGalli <emanuel.galli@bootcamp-upgrade.com>
2023-07-09 11:59:07 +0200

    Agrego calculador de intereses y finalizo el proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
+import Button from "./components/Button";
+import { formatearDinero, calcularTotalPagar } from "./utils";
 
 function App() {
   const [cantidad, setCantidad] = useState(10000);
+  const [meses, setMeses] = useState(6);
+  const [total, setTotal] = useState(0);
+  const [pago, setPago] = useState(0);
+
+  useEffect(() => {
+    const resultadoTotalPagar = calcularTotalPagar(cantidad, meses);
+    setTotal(resultadoTotalPagar);
+  }, [cantidad, meses]);
+
+  useEffect(() => {
+    setPago(total/meses);
+  }, [total]);
 
   const MIN = 0;
   const MAX = 20000;
@@ -31,29 +45,17 @@ function App() {
   }
   return (
     <>
-      <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+      < div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
         <Header />
 
         <div className="flex justify-between my-6">
-          <button
-            type="button"
-            className="h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500 "
-            onClick={handleClickDecremento}
-          >
-            -
-          </button>
-          <button
-            type="button"
-            className="h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500 "
-            onClick={handleClickIncremento}
-          >
-            +
-          </button>
+          <Button operador="-" fn={handleClickDecremento} />
+          <Button operador="+" fn={handleClickIncremento} />
         </div>
 
         <input
           type="range"
-          className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+          className="w-full h-6 bg-gray-50 accent-lime-500 hover:accent-lime-600"
           onChange={handleChange}
           min={MIN}
           max={MAX}
@@ -62,8 +64,32 @@ function App() {
         />
 
         <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">
-          {cantidad}
+          {formatearDinero(cantidad)}
         </p>
+        <h2 className="text-2xl font-extrabold text-gray-500 text-center">
+          Elige un <span className="text-indigo-600">Plazo</span> a Pagar
+        </h2>
+
+        <select className="mt-5 w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500 "
+        value={meses}
+        onChange={e => setMeses(+e.target.value)}
+        >
+          <option value="6"> 6 Meses</option>
+          <option value="12"> 12 Meses</option>
+          <option value="18"> 18 Meses</option>
+          <option value="24"> 24 Meses</option>
+          <option value="48"> 48 Meses</option>
+          <option value="56"> 56 Meses</option>
+        </select>
+
+        <div className="my-5 space-y-3 bg-gray-50 p-5">
+        <h2 className="text-2xl font-extrabold text-gray-500 text-center">
+          Resumen<span className="text-indigo-600"> de Pagos</span>
+        </h2>
+        <p className="text-xl text-gray-500 text-center font-bold "> {meses} Meses</p>
+        <p className="text-xl text-gray-500 text-center font-bold "> {formatearDinero(total)} Total a Pagar</p>
+        <p className="text-xl text-gray-500 text-center font-bold ">{formatearDinero(pago)} Pago Mensual</p>
+      </div>
       </div>
     </>
   );
